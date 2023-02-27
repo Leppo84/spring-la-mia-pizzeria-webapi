b0477de57b568b182f1fffd846a11b1704115a2b
LeppoXena <francesco.lepore@xenagames.com>
2023-02-27 15:22:06 +0100

    Pizza list displayed with api

--- src/main/java/la/mia/pizzeria/controller/api/ApiPizzasController.java
+++ src/main/java/la/mia/pizzeria/controller/api/ApiPizzasController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,33 +31,11 @@ public class ApiPizzasController {
 	@Autowired
 	IngredientRepository IngredientRepository;
 	
-//	@GetMapping
-//	public String index(@RequestParam(name = "input", required = false) String input, Model model) {
-//		List<Pizza> elencoPizze;	
-//		if (input != null && !input.isEmpty()) {
-//			elencoPizze = PizzaRepository.findByNameLike("%" + input + "%");
-//		} else {
-//			elencoPizze = PizzaRepository.findAll();
-//		}
-//		model.addAttribute("elencoPizze", elencoPizze);
-//		return "index";
-//	}
-	
 	@GetMapping()
 	public List<Pizza> index() {
 		return PizzaRepository.findAll();
 	}
 	
-//	@GetMapping("/{id}")
-//	public String singlePizza(@PathVariable("id") Integer id, Model model) {
-//		Pizza detailPizza = PizzaRepository.getReferenceById(id);
-//		
-//		model.addAttribute("pizza", detailPizza);
-//
-//		return "show";
-//
-//	}
-
 	@GetMapping("{id}")
 	public ResponseEntity<Pizza> detail(@PathVariable("id") Integer id) {
 		Optional<Pizza> res=PizzaRepository.findById(id);
@@ -64,84 +45,26 @@ public class ApiPizzasController {
 	    	return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
 	}
 	
-	
-//	@GetMapping("/create")		//gestirà le richieste GET di tipo /create
-//	public String create(Model model) {
-//		Pizza pizza=new Pizza();	//non esiste ancora sul DB
-//		pizza.setPhoto("https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg");
-//		model.addAttribute("pizza", pizza);
-//		
-//		return "/create";
-//	}
-	
-//	@PostMapping("/create")  	//gestirà le richieste di tipo POST di tipo /create
-//	public String store(
-//		@Valid @ModelAttribute("pizza") Pizza formPizza, 
-//		BindingResult bindingResult,
-//		Model model){
-//		
-//		if (bindingResult.hasErrors()) {
-//			model.addAttribute("error", "Qualcosa è andato storto :(");
-//			return "/create";
-//			}
-//		
-//		PizzaRepository.save(formPizza);
-//		
-//		return "redirect:/"; //genera un altro get
-//		
-//	}
-	
-//	@GetMapping("/edit/{id}")		//richieste GET del tipo /edit/xx
-//	public String edit(@PathVariable("id") Integer id, Model model) {		
-//		Pizza pizza=PizzaRepository.getReferenceById(id);  //lo recupero dal DB
-//
-//		List<Ingredient> elencoIngredienti=IngredientRepository.findAll();
-//		
-//		model.addAttribute("pizza", pizza);
-//		model.addAttribute("elencoIngredienti", elencoIngredienti);
-//		model.addAttribute("edit", true);
-//		return "/edit";
-//	}
-		
-//	@PostMapping("/edit/{id}")		//richieste POST del tipo /edit/n
-//	public String update(
-//			@Valid @ModelAttribute Pizza formPizza,
-//			BindingResult bindingResult,
-//			Model model) {
-//		
-//		if (bindingResult.hasErrors())
-//			return "/edit";
-//		
-////		if (PizzaRepository.findByName(formPizza.getName()).size()>0)
-////			System.out.println("Pizza già esistente");
-//		
-//		else
-//			PizzaRepository.save(formPizza);
-//		
-//		return "redirect:/";
-//	}
-	
-	@DeleteMapping("{id}")	//eliminazione   Richiesta DELETE /xx
+	@DeleteMapping("{id}")
 	public void delete(
 			@PathVariable("id") Integer id) {
 		PizzaRepository.deleteById(id);
 	}
 
-//	------------------------------------------------
 	
-//	@PostMapping()
-//	public Book create(@RequestBody Book book) {
-//		return bookRepository.save(book);
-//	}
-//	
-//	@PutMapping("{id}")	//aggiornamento   Richiesta PUT /books/xx
-//	public Book update(@RequestBody Book book,
-//			@PathVariable("id") Integer id) {
-//		Book b=bookRepository.getReferenceById(id);
-//		b.setTitle(book.getTitle());
-//		//....
-//		return bookRepository.save(b);
-//	}
+	@PostMapping()
+	public Pizza create(@RequestBody Pizza pizza) {
+		return PizzaRepository.save(pizza);
+	}
+	
+	@PutMapping("{id}")
+	public Pizza update(@RequestBody Pizza pizza,
+			@PathVariable("id") Integer id) {
+		Pizza p=PizzaRepository.getReferenceById(id);
+		p.setName(pizza.getName());
+		
+		return PizzaRepository.save(p);
+	}
 	
 	
 }
---
+++ src/main/resources/templates/api/index.js
@@ -0,0 +1,55 @@
+
+pizzaList();
+
+function pizzaList() {
+	axios.get('http://localhost:8080/api').then((res) => {
+		//codice da eseguire se la richiesta è andata a buon fine
+		console.log('richiesta ok', res);
+		document.querySelector('#pizza_table').innerHTML = '';
+		res.data.forEach(pizza => {
+			console.log(pizza);
+			document.querySelector('#pizza_table').innerHTML += `
+            <tr>
+				<td scope="col">${pizza.id}</td>
+				<td scope="col">${pizza.name}</td>
+				<td scope="col">${pizza.description}</td>
+				<td scope="col">${pizza.price}</td>
+				<td scope="col">
+					<img src="${pizza.photo}" class="col-5 img-fluid img-thumbnail">
+				</td>
+                <td>
+                    <a class="btn btn-primary" href="./edit.html?id=${pizza.id}">
+                        <i class="fa-solid fa-pen-to-square"> </i>
+                    </a>
+                </td>     
+                <td>
+                    <a class="btn btn-primary" onclick="deleteBook(${pizza.id})">
+                        <i class="fa-solid fa-trash-can"> </i>
+                    </a>
+                </td>      
+							<th scope="col">TO DO</th>   
+            </tr>
+            `
+		});
+	}).catch((res) => {
+		//codice da eseguire se la richiesta non è andata a buon fine
+		console.error('errore nella richiesta', res);
+		alert('Errore durante la richiesta!');
+	})
+}
+
+
+//function deleteBook(bookId) {
+//    const risposta=confirm('Sei sicuro?');
+//
+//    if (risposta) {
+//        axios.delete('http://localhost:8080/books/'+bookId).then((res) => {
+//            //ok => ricarico l'elenco dei libri
+//            bookList();
+//        }).catch((res) => {
+//            console.error('errore nella richiesta', res);
+//            alert('Errore durante la richiesta!');
+//        });
+//    }
+//
+//}
